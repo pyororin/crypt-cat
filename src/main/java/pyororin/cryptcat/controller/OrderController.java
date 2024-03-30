0fdb8d0858c151d0f2cf27fa967e6bc10f399fc0
shutaked <shutaked@lycorp.co.jp>
2024-03-30 23:10:13 +0900

    /order/sellを追加

--- src/main/java/pyororin/cryptcat/controller/OrderController.java
+++ src/main/java/pyororin/cryptcat/controller/OrderController.java
@@ -1,26 +1,34 @@
 package pyororin.cryptcat.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pyororin.cryptcat.controller.model.OrderRequest;
 
+import static net.logstash.logback.argument.StructuredArguments.value;
+
 @RestController
+@Slf4j
 public class OrderController {
 
     @PostMapping("/order/sell")
     public ResponseEntity<String> sell(@RequestBody OrderRequest orderRequest) {
-        String reason = orderRequest.getReason();
-        // 売買処理を行う
-        return ResponseEntity.ok("Sell order received with reason: " + reason);
+        log.info("{} {}",
+                value("kind", "access"),
+                value("Request", orderRequest.toString()));
+        // TODO: 売買処理を行う
+        return ResponseEntity.ok("OK");
     }
 
     @PostMapping("/order/buy")
     public ResponseEntity<String> buy(@RequestBody OrderRequest orderRequest) {
-        String reason = orderRequest.getReason();
-        // 売買処理を行う
-        return ResponseEntity.ok("Buy order received with reason: " + reason);
+        log.info("{} {}",
+                value("kind", "access"),
+                value("Request", orderRequest.toString()));
+        // TODO: 売買処理を行う
+        return ResponseEntity.ok("OK");
     }
 }
 
--- src/main/java/pyororin/cryptcat/controller/model/OrderRequest.java
+++ src/main/java/pyororin/cryptcat/controller/model/OrderRequest.java
@@ -1,8 +1,10 @@
 package pyororin.cryptcat.controller.model;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class OrderRequest {
     private String reason;
 }

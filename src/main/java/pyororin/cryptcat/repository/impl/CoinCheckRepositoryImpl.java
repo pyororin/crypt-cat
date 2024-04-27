73b4c65013eedf3a914f81580c907c6b381289f7
pyororin <shunichi1014@gmail.com>
2024-04-28 00:47:52 +0900

    リトライ処理とエラー時の出力を追加

--- src/main/java/pyororin/cryptcat/repository/impl/CoinCheckRepositoryImpl.java
+++ src/main/java/pyororin/cryptcat/repository/impl/CoinCheckRepositoryImpl.java
@@ -169,8 +169,8 @@ public class CoinCheckRepositoryImpl implements CoinCheckRepository {
                         value("kind", "api"), value("uri", req.getURI()), value("status", "ok"), value("id", id),
                         value("response", new Scanner(res.getBody()).useDelimiter("\\A").next().replaceAll("\\r\\n|\\r|\\n", ""))))
                 .onStatus(HttpStatusCode::isError, (req, res) -> {
-                    log.error("{} {} {} {}",
-                            value("kind", "api"), value("status", res.getStatusText()), value("id", id),
+                    log.error("{} {} {} {} {}",
+                            value("kind", "api"), value("uri", req.getURI()), value("status", res.getStatusText()), value("id", id),
                             value("response", new Scanner(res.getBody()).useDelimiter("\\A").next().replaceAll("\\r\\n|\\r|\\n", "")));
                     throw new RestClientException(res.getStatusCode().toString());
                 })
@@ -191,8 +191,8 @@ public class CoinCheckRepositoryImpl implements CoinCheckRepository {
                 })
                 .body(jsonBody.toString())
                 .retrieve()
-                .onStatus(HttpStatusCode::is2xxSuccessful, (req, res) -> log.info("{} {} {} {}",
-                        value("kind", "api"), value("status", "ok"), value("request-body", jsonBody.toString()),
+                .onStatus(HttpStatusCode::is2xxSuccessful, (req, res) -> log.info("{} {} {} {} {}",
+                        value("kind", "api"), value("uri", req.getURI()), value("status", "ok"), value("request-body", jsonBody.toString()),
                         value("response", new Scanner(res.getBody()).useDelimiter("\\A").next().replaceAll("\\r\\n|\\r|\\n", ""))))
                 .onStatus(HttpStatusCode::isError, (req, res) -> {
                     log.error("{} {} {} {} {}",

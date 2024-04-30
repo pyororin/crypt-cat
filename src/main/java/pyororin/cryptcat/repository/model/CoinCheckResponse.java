5788167b6b716505f80a02258a2725ed95005568
shutaked <shutaked@lycorp.co.jp>
2024-04-30 13:47:28 +0900

    Responseオブジェクトの変換を修正

--- src/main/java/pyororin/cryptcat/repository/model/CoinCheckResponse.java
+++ src/main/java/pyororin/cryptcat/repository/model/CoinCheckResponse.java
@@ -12,10 +12,10 @@ public class CoinCheckResponse {
     /* {"datetime":"2024-03-31T18:52:50.271","version":"1","message":"{\"success\":true,\"id\":6306219886,\"amount\":null,\"rate\":null,\"order_type\":\"market_buy\",\"pair\":\"btc_jpy\",\"created_at\":\"2024-03-31T09:52:50.000Z\",\"market_buy_amount\":\"532805.3\",\"time_in_force\":\"good_til_cancelled\",\"stop_loss_rate\":null}" */
     private String success;
     private String error;
-    private int id;
+    private long id;
     private String rate;
     @JsonProperty("market_buy_amount")
-    private String market_buy_amount;
+    private String marketBuyAmount;
     private String amount;
     @JsonProperty("order_type")
     private String orderType;

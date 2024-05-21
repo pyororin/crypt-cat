0f2849a9349d1585244bb6ff53bb51dd064fbd8f
pyororin <shunichi1014@gmail.com>
2024-05-21 19:04:53 +0900

    HashMapの初期化を設定

--- src/main/java/pyororin/cryptcat/config/OrderControlConfiguration.java
+++ src/main/java/pyororin/cryptcat/config/OrderControlConfiguration.java
@@ -3,11 +3,13 @@ package pyororin.cryptcat.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+
 @Configuration
 public class OrderControlConfiguration {
 
     @Bean
     OrderTransactions orderTransactions() {
-        return OrderTransactions.builder().build();
+        return OrderTransactions.builder().orderTransactions(new HashMap<>()).build();
     }
 }
